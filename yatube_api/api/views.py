a14d77e70fedd8fd7976a2ccf2bf7dfa8668af47
Роман <bespalovroma1995@gmail.com>
2024-01-18 19:37:04 +0300

    fixed bugs after first review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from posts.models import Group, Post
 
@@ -10,7 +11,7 @@ from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [AuthorOrReadOnly]
+    permission_classes = [IsAuthenticated, AuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -23,7 +24,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [AuthorOrReadOnly]
+    permission_classes = [IsAuthenticated, AuthorOrReadOnly]
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
